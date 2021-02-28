c6a00388390ab258019871504135f9e9a1893ef5
ddwayne <652507565@qq.com>
2021-02-28 22:40:21 +0800

    初始化router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>导航栏 |
+    <router-link to="/">Wayne</router-link> |
+  <router-link to="/xxx">Wayne2</router-link>
+  </div>
+  <hr/>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import {createWebHashHistory, createRouter} from 'vue-router'
+import Wayne from './components/Wayne.vue'
+import Wayne2 from './components/Wayne2.vue'
+
+const history = createWebHashHistory()
+const router = createRouter({
+    history: history,
+    routes:[
+        {
+            path: '/',
+            component: Wayne
+        },
+        {
+            path:'/xxx',
+            component: Wayne2
+        }
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')

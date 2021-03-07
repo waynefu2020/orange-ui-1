99d6bc84c13d180c9410df3fa92ca53a9ddf4cda
ddwayne <652507565@qq.com>
2021-03-07 21:46:40 +0800

    Doc组件整体布局

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -27,8 +27,11 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
-  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
   z-index: 10;
+  width: 100%;
   justify-content: center;
   align-items: center;
   > .logo{
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,26 +1,28 @@
 <template>
-  <Topnav/>
-  <div class="content">
-    <aside v-if="menuVisible">
-      <h2>组件列表</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs组件</router-link>
-        </li>
-      </ol>
-    </aside>
-    <main>
-      <router-view/>
-    </main>
+  <div class="layout">
+    <Topnav class="nav"/>
+    <div class="content">
+      <aside v-if="menuVisible">
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -39,10 +41,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.layout{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav{
+    flex-shrink: 0;
+  }
+  > .content{
+    flex-grow: 1;
+    border: 1px solid blue;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content{
+  display: flex;
+  > aside{ flex-shrink: 0; }
+  > main{
+    flex-grow: 1;
+    padding-left: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -52,12 +85,7 @@ aside {
     }
   }
 }
-@media (max-width: 500px) {
-  aside{
-    position: fixed;
-    left: 0;
-    top: 0;
-    padding-top: 70px;
-  }
+main{
+  overflow: auto;
 }
 </style>

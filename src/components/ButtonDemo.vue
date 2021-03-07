bff0eb2bd4931c761774d0bb7b2ae6d4bd985f5c
ddwayne <652507565@qq.com>
2021-03-07 22:00:36 +0800

    基本完成官网框架

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Button</div>
+</template>
+
+<script lang="ts">
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/DocDemo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Doc默认页面</div>
+</template>
+
+<script lang="ts">
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

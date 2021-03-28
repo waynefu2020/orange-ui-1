5f263294689c7ef4432ea1bfc27da9a49d5944b8
ddwayne <652507565@qq.com>
2021-03-28 10:16:02 +0800

    size css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,67 +1,101 @@
 <template>
-    <button class="orange-button"
-    :class="{[`orange-theme-${theme}`]: theme}"
-    >
-      <slot/>
-    </button>
+  <button class="orange-button"
+          :class="classes"
+  >
+    <slot/>
+  </button>
 </template>
 
 <script lang="ts">
-  export default {
-    props:{
-      theme: {
-        type: String,
-        default: 'button'
-      },
+import {computed} from 'vue';
+
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
     },
+    size: {
+      type: String,
+      default: 'normal'
+    }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`orange-theme-${theme}`]: theme,
+        [`orange-size-${size}`]: size,
+      };
+    });
+    return {classes}
   }
+};
 </script>
 
 <style lang="scss">
-  $h: 32px;
-  $blue: #40a9ff;
-  .orange-button{
-    box-sizing: border-box;
-    height: $h;
-    padding: 0 12px;
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    white-space: nowrap;
-    background: white;
-    color: #333;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
-    & + &{
-      margin-left: 8px;
-    }
-    &:hover, &:focus{
-      color: $blue;
-    }
-    &:focus{
-      outline: none;
-    }
-    &::-moz-focus-inner{
-      border: 0;
-    }
-    &.orange-theme-link{
-      border-color: transparent;
-      box-shadow: none;
-      color: $blue;
-      &:hover,&:focus{
-        color: lighten($blue,10%);
-      }
-    }
-    &.orange-theme-text{
-      color: inherit;
-      border-color: transparent;
-      box-shadow: none;
-      &:hover,&:focus{
-        background: darken(white, 5%);
-      }
+$h: 32px;
+$blue: #40a9ff;
+.orange-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: #333;
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover, &:focus {
+    color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+
+  &.orange-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
     }
   }
+
+  &.orange-theme-text {
+    color: inherit;
+    border-color: transparent;
+    box-shadow: none;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
+    &.orange-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.orange-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+}
 </style>
 

df0ed03135549550d7bb2098349daedbd89662d4
ddwayne <652507565@qq.com>
2021-03-28 21:44:41 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>Dialog</div>
+  <div>Dialog示例</div>
+  <h1>示例1</h1>
+  <Dialog/>
 </template>
 
 <script lang="ts">
-
+import Dialog from '../lib/Dialog.vue'
 export default {
-
+  components:{Dialog}
 }
 </script>
 

fb406498e109cbe3dcc706ba352107a7c991afe7
ddwayne <652507565@qq.com>
2021-04-05 17:50:26 +0800

    优化selectedItem

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,7 +3,7 @@
     <div class="orange-tabs-nav" ref="container">
       <div class="orange-tabs-nav-item"
            v-for="(t,index) in titles"
-           :ref="el => {if(el) navItems[index] = el}"
+           :ref="el => {if(t===selected) selectedItem = el}"
            @click="select(t)"
            :class="{selected: t === selected}"
            :key="index">{{ t }}
@@ -29,17 +29,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div => div.classList.contains('selected'))[0];
-      console.log(result);
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     };
@@ -64,7 +61,7 @@ export default {
     const select = (title: String) => {
       context.emit('update:selected', title);
     };
-    return {defaults, titles, current, select, navItems, indicator, container};
+    return {defaults, titles, current, select, selectedItem, indicator, container};
   }
 };
 </script>

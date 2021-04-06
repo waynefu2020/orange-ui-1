51aee0678b470899cbd80a8a3ca082e9cae7ccfa
ddwayne <652507565@qq.com>
2021-04-06 22:07:25 +0800

    引入github样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import {router} from './router';
 import './lib/orange.scss';
 import './index.scss';
+import 'github-markdown-css'
 
 const app = createApp(App);
 app.use(router);
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>安装</div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install orange-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add orange-ui</code></pre>
+
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
 </template>
